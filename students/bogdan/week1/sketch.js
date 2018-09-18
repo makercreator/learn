e9de634f9aa0479555a49b6cb5619feae5391bd9
Your Name <you@example.com>
2018-09-18 12:20:54 -0400

    update bogdan

--- students/bogdan/week1/sketch.js
+++ students/bogdan/week1/sketch.js
@@ -18,7 +18,7 @@ function setup() {
   createCanvas(800, 400);
   stroke(150);
   noLoop();  // Run once and stop
- 
+
 
   var rowCount=skills.getRowCount()-1; // number of rows -1 to ignore last value (overall)
   for(var i=0; i<rowCount; i++) {
@@ -37,29 +37,28 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  background(50);
   noStroke();
   pieChart(diameter, angles);
-  fill(100);
-  ellipse(width/4,height/2,250,250);
+  fill(50);
+  ellipse(width/1.5,height/2,250,250);
 
 //OVERALL PERCENT//
   fill(150);
   textAlign(CENTER);
   textSize(80);
-  text(skills.getString(overall,1), 200,190);
+  text(skills.getString(overall,1), width/1.5,250);
 
-  
+
 
 // STUDENT NAME & TITLE //
   textAlign(CENTER);
   fill(200);
   textSize(24);
-  text(skills.getString(1,4), 200,260);
   textSize(12);
-  text(skills.getString(2,4), 200,235);
-  
-  
+  text(skills.getString(2,4), width/1.5,170);
+
+
 }
 
 
@@ -68,19 +67,19 @@ function pieChart(diameter, data) {
   //change color / slice//
   for (var i = 0; i < data.length; i++) {
 
- 
+
   //100%//
   if(value[i] == 100) {
     fill(0, 255, 150, 255);
   }
-  
+
   // 0% //
   if(value[i] < 1) {
     fill(150);
   }
 
   //draw slices//
-    arc(width/4, height/2, diameter, diameter, lastAngle, lastAngle-.02 + radians(angles[i])); // divides the pie chart based on how many rows in excell. .02 adds a gap. 
+    arc(width/1.5, height/2, diameter, diameter, lastAngle, lastAngle-.02 + radians(angles[i])); // divides the pie chart based on how many rows in excell. .02 adds a gap.
     lastAngle += radians(angles[i]); //each slice starts where the last left off.
   }
 
@@ -88,20 +87,20 @@ beginShape();
 
 
 for(var i=0; i<value.length; i++) {
-  
+
     var x=map(17, 10, 100 , 0, 140);
     var y=map(i, 0, value.length, 20, 400);//y position of progress bar
     print(value[i]);
-  
-  var c=skills.getNum(i,1); 
+
+  var c=skills.getNum(i,1);
     if(value[i] < 100) {
-    stroke(255)  
+    stroke(255)
     fill(0, 200, 255, 255);
   }
 
   //100%//
   if(value[i] == 100) {
-    stroke(0, 255, 150, 255); 
+    stroke(0, 255, 150, 255);
     fill(0, 255, 150, 255);
   }
 
@@ -110,19 +109,18 @@ for(var i=0; i<value.length; i++) {
     fill(150);
   }
 
-  rect(400, y, x+1, 12);x
-    
-    
+  rect(50, y, x+1, 12);x
+
+
 }
   endShape()
-  
+
   for(var i=0; i<value.length; i++) {
 
   var y=map(i, 0, value.length, 20, 400);//y position of progress bar descriptors
   textSize(10);
   noStroke();
   fill(255);
-  text(skills.getString(i,0), 430, y+10);
+  text(skills.getString(i,0), 80, y+10);
   }
 }
-  
